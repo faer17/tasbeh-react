b29c8a080086fd7eb3eb4b1666283049dd3b249d
faer17 <faerumidovna17@gmail.com>
2023-06-09 16:10:31 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css'
 
 export default function App() {
     const [count,setCount]=useState(0)
+    if (count > 101) {
+      setCount  (100)
+    }
     return(
     <>
       <section>
         <div className="tasbeh">
         <span></span>
         <h1>{count}</h1>
-        <button className="count" onClick={() => setCount(count === 100 ? count === 0 :  count + 1)}>count</button>
-        <button className="reset" onClick={() => setCount(count === 0)}>reset</button>
+        <button className="count" onClick={() => setCount(count === 100 ? count - count :  count + 1)}>count</button>
+        <button className="reset" onClick={() => setCount(count - count )}>reset</button>
         </div>
-
       </section>
     </>
     )
 }
+
